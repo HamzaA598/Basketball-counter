b6689a4fccae4a99ca1d0827630368b1c28716ce
HamzaA598 <hamzaemad598@gmail.com>
2019-08-14 22:55:09 +0200

    Basketball counter

---
+++ app/src/main/java/com/example/basketballcounter/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.basketballcounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mAPointsTV, mBPointsTV;
+    Button mA3PointsBtn, mA2PointsBtn, mAFTBtn, mB3PointsBtn, mB2PointsBtn, mBFTBtn, mResetBtn;
+
+    int APoints = 0;
+    int BPoints = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mA3PointsBtn = findViewById(R.id.A3PointsBtn);
+        mA2PointsBtn = findViewById(R.id.A2PointsBtn);
+        mAFTBtn = findViewById(R.id.AFTBtn);
+        mB3PointsBtn = findViewById(R.id.B3PointsBtn);
+        mB2PointsBtn = findViewById(R.id.B2PointsBtn);
+        mBFTBtn = findViewById(R.id.BFTBtn);
+        mAPointsTV = findViewById(R.id.APointsTV);
+        mBPointsTV = findViewById(R.id.BPointsTV);
+        mResetBtn = findViewById(R.id.resetBtn);
+
+        mA3PointsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                APoints += 3;
+                mAPointsTV.setText("" + APoints);
+
+            }
+        });
+
+        mA2PointsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                APoints += 2;
+                mAPointsTV.setText("" + APoints);
+            }
+        });
+
+        mAFTBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                APoints += 1;
+                mAPointsTV.setText("" + APoints);
+            }
+        });
+
+        mB3PointsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BPoints += 3;
+                mBPointsTV.setText("" + BPoints);
+            }
+        });
+
+        mB2PointsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BPoints += 2;
+                mBPointsTV.setText("" + BPoints);
+            }
+        });
+
+        mBFTBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BPoints += 1;
+                mBPointsTV.setText("" + BPoints);
+            }
+        });
+
+        mResetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                APoints = 0;
+                BPoints = 0;
+                mAPointsTV.setText("" + APoints);
+                mBPointsTV.setText("" + BPoints);
+            }
+        });
+
+    }
+
+}
